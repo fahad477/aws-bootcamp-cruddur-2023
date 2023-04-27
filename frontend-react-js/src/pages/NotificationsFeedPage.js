726e8e1523f56715d4de725e81de689216551bf3
Fahad  Saeed <fahad.saeed@zohomail.eu>
2023-04-27 23:50:58 +0000

    fix

--- frontend-react-js/src/pages/NotificationsFeedPage.js
+++ frontend-react-js/src/pages/NotificationsFeedPage.js
@@ -20,7 +20,7 @@ export default function HomeFeedPage() {
 
   const loadData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/notifications`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/NotificationsFeedPage`
       const res = await fetch(backend_url, {
         method: "GET"
       });
